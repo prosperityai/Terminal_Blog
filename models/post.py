6012c37f541dc5b76274f12bef19f13c2e0f5428
prosperityai <prosperpaul@ymail.com>
2017-08-29 08:55:54 +0300

    adding datetime module

--- models/post.py
+++ models/post.py
@@ -1,12 +1,16 @@
+import uuid
+from database import Database
+import datetime
 class Post(object):
   """docstring for Post"""
-  def __init__(self, blog_id, title,content,author,idt,date):
+  def __init__(self, blog_id, title,content,author,date = datetime.datetime.utcnow(),id=None):
      self.blog_id = blog_id
     self.title = title
     self.content = content
     self.author = author
-    self.idt = idt
     self.created_date = date
+    self.idt = uuid.uuid4().hexa if id is None else id
+
 
   def mongo_save(self):
     Database.insert(collections = 'posts',data = self.json)
@@ -24,9 +28,9 @@ class Post(object):
 
 
   @staticmethod
- def from_mongo(idt):
-   return Database.find_one(collections='posts',query ={'idt':idt})
+ def from_mongo(id):
+   return Database.find_one(collections='posts',query ={'id':id})
 
    @staticmethod
- def from_mongo(idt):
-   return [post for post in Database.find(collections='posts',query ={'blog_id':idt})]
+ def from_mongo(id):
+   return [post for post in Database.find(collections='posts',query ={'blog_id':id})]
